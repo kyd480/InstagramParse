34e142ca527187ccf2c63cbcee098792130d78d3
Kevin Duong <kdy480@yahoo.com>
2016-03-01 15:49:02 -0500

    first commit

--- InstagramParse/AppDelegate.swift
+++ InstagramParse/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Initialize Parse
+        // Set applicationId and server based on the values in the Heroku settings.
+        // clientKey is not used on Parse open source unless explicitly configured
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Instagram"
+                configuration.clientKey = "aldskfjnsladkflkdsanfdsklfjs"
+                configuration.server = "https://blooming-thicket-90726.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'InstagramParse' do
+    pod 'Parse'
+end
+
+target 'InstagramParseTests' do
+
+end
+
+target 'InstagramParseUITests' do
+
+end
+
